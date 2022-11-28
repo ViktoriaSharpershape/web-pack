f0a33abfb36e1fcbe4595ddbc73d03806dc10c4c
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2022-11-28 14:38:49 +0200

    first commit

---
+++ my-first-webpack/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+          }
+    ]
+    
+  }
+}
+module.exports = config;
