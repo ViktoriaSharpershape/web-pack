18a02c8b4c7fa173d09a7f89c7c08e9291a3d22c
ViktoriaSharpershape <viktoriia.shevchenko@sharpershape.com>
2022-11-29 14:27:31 +0200

    added CSS styling

--- my-first-webpack/webpack.config.js
+++ my-first-webpack/webpack.config.js
@@ -18,6 +18,13 @@ const config = {
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             use: 'babel-loader'
+          },
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
           }
     ]
     
